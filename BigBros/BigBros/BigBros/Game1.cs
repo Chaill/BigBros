4e84480db33ffb44f03a580292f0af655f3bb2c2
ChailKim <chailworld@gmail.com>
2016-03-23 16:39:58 +0900

    add mouse visible

--- BigBros/BigBros/BigBros/Game1.cs
+++ BigBros/BigBros/BigBros/Game1.cs
@@ -19,6 +19,8 @@ namespace BigBros
         
         public Game1()
         {
+            IsMouseVisible = true;
+            
             m_Graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
